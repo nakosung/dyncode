7279c7f4b1db97aed93d007641aa6c1fdd458c21
성낙호 <deif@redduck.com>
2014-02-11 11:20:03 +0900

    parameters are parsed from the first line of document

--- dyncode.py
+++ dyncode.py
@@ -2,11 +2,10 @@ import sublime, sublime_plugin
 
 class DynCodeListener(sublime_plugin.EventListener):
 	def on_post_save(self,view):		
-		if view.file_name() and view.substr(view.line(0)) == '#LIVE':
-			file_name = view.file_name().split('/')[-1]
-			ext = file_name.split('.')[-1]
-			if ext == 'coffee':
-				execute = ("curl http://localhost:8080/coffee/" + file_name + " -X POST --data-binary @" + view.file_name()).split(' ')
+		if view.file_name() :
+			first_line = view.substr(view.line(0)).split(' ')
+			if first_line[0] == '#DYNCODE' :
+				file_name = view.file_name().split('/')[-1]
+				execute = ("curl " + first_line[1] + "/" + file_name + " -X POST --data-binary @" + view.file_name()).split(' ')
 				view.window().run_command("exec",{"cmd":execute})
-
 
