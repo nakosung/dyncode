8fa2bc50f9d6272d78ac07794d3b835f8456734b
성낙호 <deif@redduck.com>
2014-02-11 11:28:45 +0900

    supports windows file name

--- dyncode.py
+++ dyncode.py
@@ -4,6 +4,7 @@ class DynCodeListener(sublime_plugin.EventListener):
 	def on_post_save(self,view):		
 		if view.file_name() :
 			first_line = view.substr(view.line(0)).split(' ')
+			first_line = first_line.replace('\\','/')
 			if first_line[0] == '#DYNCODE' :
 				file_name = view.file_name().split('/')[-1]
 				execute = ("curl " + first_line[1] + "/" + file_name + " -X POST --data-binary @" + view.file_name()).split(' ')
